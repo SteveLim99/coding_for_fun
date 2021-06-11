bc5e70450cef4786e7a2603928389987479b6cb5
SteveLim99 <stevelimzilian@gmail.com>
2021-06-11 14:34:21 +0800

    ctci 1.2 #1

--- src/ctci/chp_1.java
+++ src/ctci/chp_1.java
@@ -1,9 +1,14 @@
 package ctci;
 
+import java.util.HashMap;
+
 public class chp_1 {
 
     public static void main(String[] args){
-        boolean res = is_unique_1_dot_1("tes");
+//        boolean res = is_unique_1_dot_1("tes");
+//        System.out.println(res);
+
+        boolean res = check_permutation_1_dot_2("test", "tats");
         System.out.println(res);
     }
 
@@ -20,4 +25,25 @@ public class chp_1 {
 
         return true;
     }
+
+    private static boolean check_permutation_1_dot_2(String a, String b){
+        if (a.length() != b.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < a.length(); i++){
+            char curr_a = a.charAt(i);
+            char curr_b = b.charAt(i);
+
+            map.put(curr_a, map.getOrDefault(curr_a, 0) + 1);
+            map.put(curr_b, map.getOrDefault(curr_b, 0) + 1);
+        }
+
+        for (char c : map.keySet()){
+            if (map.get(c) % 2 != 0)
+                return false;
+        }
+        return true;
+    }
+
+
 }
