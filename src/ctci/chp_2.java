412a2eee36164477a2414bdc8a190d897fde0f10
SteveLim99 <stevelimzilian@gmail.com>
2021-06-12 14:02:14 +0800

    ctci 2.2 #1

--- src/ctci/chp_2.java
+++ src/ctci/chp_2.java
@@ -9,6 +9,14 @@ import java.util.Queue;
 
 public class chp_2 {
 
+//    public static void main(String[] args){
+//        ListNode in = new ListNode(1);
+//        in.next = new ListNode(2);
+//        in.next.next = new ListNode(3);
+//        ListNode res = kth_last_2_dot_2(in,2);
+//        System.out.println(res.val);
+//    }
+
     /*
         Usage of a map to mantain occurence and a queue to mantain the order of elements within the linked list.
         Time Complexity: O(n)
@@ -69,5 +77,24 @@ public class chp_2 {
         }
 
         return res.next;
+    }
+
+    /*
+        Store index of each node in the linked list in a hashmap
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+     */
+    public static ListNode kth_last_2_dot_2(ListNode head, int k){
+        Map<Integer, ListNode> map = new HashMap<>();
+        ListNode pointer = head;
+        int idx = 0;
+
+        while(pointer != null){
+            map.put(idx, pointer);
+            pointer = pointer.next;
+            idx++;
+        }
+
+        return (idx - k - 1>= 0) ? map.get(idx - k - 1) : map.get(0);
     }
 }
