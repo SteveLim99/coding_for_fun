c5710b5bf287952a59dae7d4e543b7c21fe48afd
SteveLim99 <stevelimzilian@gmail.com>
2021-06-12 14:14:39 +0800

    leetcode 206 #1

--- src/leetcode/easy.java
+++ src/leetcode/easy.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import utils.ListNode;
+
 public class easy {
 
     private static int idx_26(int[] nums) {
@@ -11,5 +13,29 @@ public class easy {
             }
         }
         return idx;
+    }
+
+    /*
+        Title: Reverse Linked List
+        Solution: Create a new linked list and use a pointer to reference to new head of new linked list and finally return the final head.
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        Link: https://leetcode.com/problems/reverse-linked-list/
+     */
+    private static ListNode idx_206(ListNode head){
+        ListNode res = null;
+        if(head != null){
+            res = new ListNode(head.val);
+            head = head.next;
+        }
+
+        while(head != null){
+            ListNode tmp = new ListNode(head.val);
+            tmp.next = res;
+            res = tmp;
+            head = head.next;
+        }
+
+        return res;
     }
 }
