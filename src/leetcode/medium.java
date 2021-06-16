8df7ac676b6886e888133f9881b8b52631572ebf
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2021-06-16 23:23:38 +0800

    leetcode 199 #1

--- src/leetcode/medium.java
+++ src/leetcode/medium.java
@@ -2,7 +2,9 @@ package leetcode;
 
 import utils.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class medium {
@@ -13,7 +15,7 @@ public class medium {
         Space Complexity: O(n)
         Link: https://leetcode.com/problems/deepest-leaves-sum/
      */
-    public int deepestLeavesSum(TreeNode root) {
+    public int idx_1302_0(TreeNode root) {
         Queue<TreeNode> bfs = new LinkedList();
         int sum = 0;
         if(root != null) bfs.add(root);
@@ -33,5 +35,32 @@ public class medium {
             sum = level_sum;
         }
         return sum;
+    }
+
+    /*
+        Title: Binary Tree Right Side View
+        Solution: Given the root of a binary tree, imagine yourself standing on the right side of it,
+                  return the values of the nodes you can see ordered from top to bottom.
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        Link: https://leetcode.com/problems/binary-tree-right-side-view/
+     */
+    public List<Integer> idx_199_0(TreeNode root) {
+        List<Integer> res = new ArrayList();
+        Queue<TreeNode> bfs = new LinkedList();
+        if(root != null) bfs.add(root);
+
+        while(!bfs.isEmpty()){
+            int count = bfs.size();
+
+            for(int i = 0; i < count; i++){
+                TreeNode tmp = bfs.poll();
+
+                if(i == count - 1) res.add(tmp.val);
+                if(tmp.left != null) bfs.add(tmp.left);
+                if(tmp.right != null) bfs.add(tmp.right);
+            }
+        }
+        return res;
     }
 }
